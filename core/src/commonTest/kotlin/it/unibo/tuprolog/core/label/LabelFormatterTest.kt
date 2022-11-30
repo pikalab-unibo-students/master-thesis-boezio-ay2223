5ad3aaffdad500f768921994f3d5e53c49c35e69
giuseppeboezio <giuseppe.boezio@studio.unibo.it>
2022-11-30 11:47:08 +0100

    feat: class test for LabelFormatter

---
+++ core/src/commonTest/kotlin/it/unibo/tuprolog/core/label/LabelFormatterTest.kt
@@ -0,0 +1,44 @@
+package it.unibo.tuprolog.core.label
+
+import it.unibo.tuprolog.core.Atom
+import it.unibo.tuprolog.core.Struct
+import it.unibo.tuprolog.core.format
+import it.unibo.tuprolog.solve.LabelAwareTermFormatter
+import it.unibo.tuprolog.solve.addLabel
+import it.unibo.tuprolog.solve.equalsWithLabels
+import it.unibo.tuprolog.solve.labels
+import kotlin.test.Test
+
+class LabelFormatterTest {
+
+    @Test
+    fun testFormatterLabelledStruct() {
+        val term: Struct = Struct.of("f", Atom.of("a"))
+
+        println(term)
+        println(term.labels)
+        println(term.format(LabelAwareTermFormatter))
+
+        val term1: Struct = term.addLabel("pippo")
+        println(term1)
+        println(term1.labels)
+        println(term1.format(LabelAwareTermFormatter))
+
+        println(term == term1)
+        println(term.equalsWithLabels(term1))
+
+    }
+
+    @Test
+    fun testFormatterLabelledStructAndLabelledArgs() {
+        val term: Struct = Struct.of(
+            "f",
+            Atom.of("a").addLabel("x")
+        ).addLabel("y")
+
+        println(term)
+        println(term.labels)
+        println(term.format(LabelAwareTermFormatter))
+
+    }
+}
