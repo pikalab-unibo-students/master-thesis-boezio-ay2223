07c694b6b362d5005ea7f2aa5f75dc3e64b797d9
giuseppeboezio <giuseppe.boezio@studio.unibo.it>
2022-11-30 13:57:00 +0100

    chores(LabelTest): change structure of test class

--- core/src/commonTest/kotlin/it/unibo/tuprolog/core/label/LabelTest.kt
+++ core/src/commonTest/kotlin/it/unibo/tuprolog/core/label/LabelTest.kt
@@ -7,24 +7,6 @@ import kotlin.test.Test
 class LabelTest {
 
     @Test
-    fun labelsExample() {
-        val term: Struct = Struct.of("f", Atom.of("a"))
-
-        println(term)
-        println(term.labels)
-        println(term.format(LabelAwareTermFormatter))
-
-        val term1: Struct = term.addLabel("pippo")
-        println(term1)
-        println(term1.labels)
-        println(term1.format(LabelAwareTermFormatter))
-
-        println(term == term1)
-        println(term.equalsWithLabels(term1))
-
-    }
-
-    @Test
     fun testChangeLabelsInplace(){
 
         val term: Struct = Struct.of("f", Atom.of("a"))
