c870fa7d984b08bb2d5ff1acb92f2b60266a6493
giuseppeboezio <giuseppe.boezio@studio.unibo.it>
2022-12-18 11:47:03 +0100

    fix: labels of the outer struct are now updated

--- unify-labels/src/commonMain/kotlin/it/unibo/tuprolog/unify/label/AbstractLabelledUnificator.kt
+++ unify-labels/src/commonMain/kotlin/it/unibo/tuprolog/unify/label/AbstractLabelledUnificator.kt
@@ -3,6 +3,7 @@ package it.unibo.tuprolog.unify.label
 import it.unibo.tuprolog.core.Substitution
 import it.unibo.tuprolog.core.Term
 import it.unibo.tuprolog.core.label.Labels
+import it.unibo.tuprolog.core.label.addLabellings
 import it.unibo.tuprolog.core.label.labels
 import it.unibo.tuprolog.core.label.setLabellings
 import it.unibo.tuprolog.unify.AbstractUnificator
@@ -36,6 +37,20 @@ abstract class AbstractLabelledUnificator(context: Substitution = Substitution.e
     override fun shouldUnify(term1: Term, labels1: Labels, term2: Term, labels2: Labels): Boolean = true
 
     override fun merge(term1: Term, labels1: Labels, term2: Term, labels2: Labels): Labels = emptySet()
+
+    override fun mgu(term1: Term, term2: Term, occurCheckEnabled: Boolean): Substitution {
+        if (shouldUnify(term1, term1.labels, term2, term2.labels)) {
+            val mgu = super<AbstractUnificator>.mgu(term1, term2, occurCheckEnabled)
+            if (mgu.isSuccess) {
+                val finalLabels = merge(term1, term1.labels, term2, term2.labels)
+                val termsMap = mutableMapOf<Term,Labels>()
+                termsMap[term1] = finalLabels
+                termsMap[term2] = finalLabels
+                return mgu.addLabellings(termsMap)
+            }
+        }
+        return Substitution.failed()
+    }
 
     override fun unify(term1: Term, term2: Term, occurCheckEnabled: Boolean): Term? {
         val mguWithLabels = mgu(term1, term2, occurCheckEnabled)
