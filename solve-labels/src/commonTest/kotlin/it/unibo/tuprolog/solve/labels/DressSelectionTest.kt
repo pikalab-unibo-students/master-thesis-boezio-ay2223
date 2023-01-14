84c25705b69142ce6fe8401d234ed02d0d815dc1
giuseppeboezio <giuseppe.boezio@studio.unibo.it>
2023-01-14 16:51:34 +0100

    style: fix indentation and comments

--- solve-labels/src/commonTest/kotlin/it/unibo/tuprolog/solve/labels/DressSelectionTest.kt
+++ solve-labels/src/commonTest/kotlin/it/unibo/tuprolog/solve/labels/DressSelectionTest.kt
@@ -21,39 +21,46 @@ class DressSelectionTest {
         val argsReference = referenceColor.args.map { it.castToInteger().value.toDouble() }
         val argsDress = dressColor.args.map { it.castToInteger().value.toDouble() }
         val intThreshold = threshold.value.toDouble()
-        val componentDistances = argsReference.zip(argsDress).map { (lhs,rhs) -> (lhs - rhs).pow(2) }
+        val componentDistances = argsReference.zip(argsDress).map { (lhs, rhs) -> (lhs - rhs).pow(2) }
         val euclideanDistance = sqrt(componentDistances.sum())
         return euclideanDistance <= intThreshold
     }
 
     @Test
-    fun testDressSelectionCaseStudy(){
-
+    fun testDressSelectionCaseStudy() {
         // Knowledge Base
         val theory = Theory.of(
             // dress ( sweater , rgb (255 , 240 , 245) ) <@winter ,@fall >.
-            Fact.of(Struct.of(
+            Fact.of(
+                Struct.of(
                 "dress",
                 Atom.of("sweater"),
-                Struct.of("rgb", Integer.of(255), Integer.of(240), Integer.of(245))
+                Struct.of("rgb", Integer.of(255), Integer.of(240), Integer.of(245)
+                )
             ).addLabel("winter").addLabel("fall")),
-            //dress ( t-shirt , rgb (255 , 222 , 173) ) <@summer , @spring >.
-            Fact.of(Struct.of(
+            // dress ( t-shirt , rgb (255 , 222 , 173) ) <@summer , @spring >.
+            Fact.of(
+                Struct.of(
                 "dress",
                 Atom.of("t-shirt"),
-                Struct.of("rgb", Integer.of(255), Integer.of(222), Integer.of(173))
+                Struct.of("rgb", Integer.of(255), Integer.of(222), Integer.of(173)
+                )
             ).addLabel("summer").addLabel("spring")),
-            //dress ( t-shirt , rgb (119 , 136 , 153) ) <@summer , @spring >.
-            Fact.of(Struct.of(
+            // dress ( t-shirt , rgb (119 , 136 , 153) ) <@summer , @spring >.
+            Fact.of(
+                Struct.of(
                 "dress",
                 Atom.of("t-shirt"),
-                Struct.of("rgb", Integer.of(119), Integer.of(136), Integer.of(153))
+                Struct.of("rgb", Integer.of(119), Integer.of(136), Integer.of(153)
+                )
             ).addLabel("summer").addLabel("spring")),
             //dress (jeans , rgb (188 , 143 , 143) ) <@winter ,@fall , @summer , @spring >.
-            Fact.of(Struct.of(
+            Fact.of(
+                Struct.of(
                 "dress",
                 Atom.of("jeans"),
-                Struct.of("rgb", Integer.of(188), Integer.of(143), Integer.of(143))
+                Struct.of("rgb", Integer.of(188), Integer.of(143), Integer.of(143)
+                )
             ).addLabel("winter").addLabel("fall").addLabel("summer").addLabel("spring")),
         )
         // query of the user
@@ -66,11 +73,11 @@ class DressSelectionTest {
         // custom unificator for this problem
         val unificator = LabelledUnificator.strict(
             shouldUnify = { term1, l1, term2, l2 ->
-                if (term1 is Struct && term2 is Struct && term1.functor == "dress" && term2.functor == "dress"){
+                if (term1 is Struct && term2 is Struct && term1.functor == "dress" && term2.functor == "dress") {
                     if (l2 == emptySet<Label>()) {
                         true
                     } else {
-                        l1.any { it == l2 }// slight variation
+                        l1.any { it == l2 } // slight variation
                     }
                 } else if (term1 is Var && l2 == emptySet<Label>()) {
                     true
@@ -85,8 +92,8 @@ class DressSelectionTest {
                 }
             },
             merge = { term1, _, term2, l2 ->
-                if(term1 is Struct && term2 is Struct && term1.functor == "dress" && term2.functor == "dress"){
-                    if(l2 == emptySet<Label>()) {
+                if(term1 is Struct && term2 is Struct && term1.functor == "dress" && term2.functor == "dress") {
+                    if (l2 == emptySet<Label>()) {
                         emptySet()
                     } else {
                         l2
