eb71cb254ff4a93035d13c94ec7d1d78474cb99c
giuseppeboezio <giuseppe.boezio@studio.unibo.it>
2023-01-18 11:39:44 +0100

    test: other tests for case study

--- solve-labels/src/commonTest/kotlin/it/unibo/tuprolog/solve/labels/DressSelectionTest.kt
+++ solve-labels/src/commonTest/kotlin/it/unibo/tuprolog/solve/labels/DressSelectionTest.kt
@@ -8,6 +8,7 @@ import it.unibo.tuprolog.core.Var
 import it.unibo.tuprolog.core.label.Label
 import it.unibo.tuprolog.core.label.addLabel
 import it.unibo.tuprolog.core.label.labels
+import it.unibo.tuprolog.solve.Solver
 import it.unibo.tuprolog.theory.Theory
 import it.unibo.tuprolog.unify.label.LabelledUnificator
 import kotlin.math.pow
@@ -17,12 +18,72 @@ import kotlin.test.assertTrue
 
 class DressSelectionTest {
 
+    // custom unificator for this problem
+    private val unificator = LabelledUnificator.strict(
+        shouldUnify = { term1, l1, term2, l2 ->
+            if (term1.let { it is Struct && it.functor == "dress" && it.arity == 2 }
+                && term2.let { it is Struct && it.functor == "dress" && it.arity == 2 }) {
+                if (l1 == emptySet<Label>()) {
+                    true
+                } else {
+                    l1.all { it in l2 }
+                }
+            } else if (term1.labels.size == 2
+                && term2.let { it is Struct && it.functor == "rgb" && it.arity == 3}) {
+                val labels = term1.labels
+                val rgbReference = labels.elementAt(0).value
+                val threshold = labels.elementAt(1).value
+                return@strict checkSimilarity(
+                    rgbReference as Struct,
+                    term2.castToStruct(),
+                    threshold as Int
+                )
+            } else {
+                true
+            }
+        },
+        merge = { term1, l1, term2, _ ->
+            if (term1.let { it is Struct && it.functor == "dress" && it.arity == 2 }
+                && term2.let { it is Struct && it.functor == "dress" && it.arity == 2 }) {
+                if (l1 == emptySet<Label>()) {
+                    emptySet()
+                } else {
+                    l1
+                }
+            } else if (term1.labels.size == 2 && term2 is Struct) {
+                l1
+            } else {
+                emptySet()
+            }
+        },
+        stillValid = { _, _ -> true }
+    )
+
+    // query of the user
+    private val goal = Struct.of(
+        "dress",
+        Var.of("Name"),
+        Var.of("Color").addLabel(Struct.of(
+            "rgb",
+            Integer.of(255),
+            Integer.of(239),
+            Integer.of(213)
+        )).addLabel(35)
+    ).addLabel("winter").addLabel("fall")
+
     private fun checkSimilarity(referenceColor: Struct, dressColor: Struct, threshold: Int): Boolean {
         val argsReference = referenceColor.args.map { it.castToInteger().value.toDouble() }
         val argsDress = dressColor.args.map { it.castToInteger().value.toDouble() }
         val componentDistances = argsReference.zip(argsDress).map { (lhs, rhs) -> (lhs - rhs).pow(2) }
         val euclideanDistance = sqrt(componentDistances.sum())
         return euclideanDistance <= threshold
+    }
+
+    private fun createSolver(theory: Theory): Solver {
+        return LabelledPrologSolverFactory.solverOf(
+            unificator = unificator,
+            staticKb = theory
+        )
     }
 
     @Test
@@ -82,63 +143,110 @@ class DressSelectionTest {
                 ).addLabel("winter").addLabel("fall").addLabel("summer").addLabel("spring")
             ),
         )
-        // query of the user
-        val goal = Struct.of(
-            "dress",
-            Var.of("Name"),
-            Var.of("Color").addLabel(Struct.of(
-                "rgb",
-                Integer.of(255),
-                Integer.of(239),
-                Integer.of(213)
-            )).addLabel(35)
-        ).addLabel("winter")
 
-        // custom unificator for this problem
-        val unificator = LabelledUnificator.strict(
-            shouldUnify = { term1, l1, term2, l2 ->
-                if (term1.let { it is Struct && it.functor == "dress" && it.arity == 2 }
-                    && term2.let { it is Struct && it.functor == "dress" && it.arity == 2 }) {
-                    if (l1 == emptySet<Label>()) {
-                        true
-                    } else {
-                        l1.all { it in l2 }
-                    }
-                } else if (term1.labels.size == 2
-                    && term2.let { it is Struct && it.functor == "rgb" && it.arity == 3}) {
-                    val labels = term1.labels
-                    val rgbReference = labels.elementAt(0).value
-                    val threshold = labels.elementAt(1).value
-                    return@strict checkSimilarity(
-                        rgbReference as Struct,
-                        term2.castToStruct(),
-                        threshold as Int
+        val solver = createSolver(theory)
+
+        val solution = solver.solveOnce(goal)
+        assertTrue(solution.isYes)
+    }
+
+    @Test
+    fun testDressSelectionCaseStudyWithoutFirstFact() {
+        // Knowledge Base
+        val theory = Theory.of(
+            // dress ( t-shirt , rgb (255 , 222 , 173) ) <@summer , @spring >.
+            Fact.of(
+                Struct.of(
+                    "dress",
+                    Atom.of("t-shirt"),
+                    Struct.of(
+                        "rgb",
+                        Integer.of(255),
+                        Integer.of(222),
+                        Integer.of(173)
                     )
-                } else {
-                    true
-                }
-            },
-            merge = { term1, l1, term2, _ ->
-                if (term1.let { it is Struct && it.functor == "dress" && it.arity == 2 }
-                    && term2.let { it is Struct && it.functor == "dress" && it.arity == 2 }) {
-                    if (l1 == emptySet<Label>()) {
-                        emptySet()
-                    } else {
-                        l1
-                    }
-                } else if (term1.labels.size == 2 && term2 is Struct) {
-                    l1
-                } else {
-                    emptySet()
-                }
-            },
-            stillValid = { _, _ -> true }
+                ).addLabel("summer").addLabel("spring")
+            ),
+            // dress ( t-shirt , rgb (119 , 136 , 153) ) <@summer , @spring >.
+            Fact.of(
+                Struct.of(
+                    "dress",
+                    Atom.of("t-shirt"),
+                    Struct.of(
+                        "rgb",
+                        Integer.of(119),
+                        Integer.of(136),
+                        Integer.of(153)
+                    )
+                ).addLabel("summer").addLabel("spring")
+            ),
+            // dress (jeans , rgb (188 , 143 , 143) ) <@winter ,@fall , @summer , @spring >.
+            Fact.of(
+                Struct.of(
+                    "dress",
+                    Atom.of("jeans"),
+                    Struct.of(
+                        "rgb",
+                        Integer.of(188),
+                        Integer.of(143),
+                        Integer.of(143)
+                    )
+                ).addLabel("winter").addLabel("fall").addLabel("summer").addLabel("spring")
+            ),
         )
 
-        val solver = LabelledPrologSolverFactory.solverOf(
-            unificator = unificator,
-            staticKb = theory
+        val solver = createSolver(theory)
+
+        val solution = solver.solveOnce(goal)
+        assertTrue(solution.isNo)
+    }
+
+    @Test
+    fun testDressSelectionUnifyLastFact() {
+        // Knowledge Base
+        val theory = Theory.of(
+            // dress ( t-shirt , rgb (255 , 222 , 173) ) <@summer , @spring >.
+            Fact.of(
+                Struct.of(
+                    "dress",
+                    Atom.of("t-shirt"),
+                    Struct.of(
+                        "rgb",
+                        Integer.of(255),
+                        Integer.of(222),
+                        Integer.of(173)
+                    )
+                ).addLabel("summer").addLabel("spring")
+            ),
+            // dress ( t-shirt , rgb (119 , 136 , 153) ) <@summer , @spring >.
+            Fact.of(
+                Struct.of(
+                    "dress",
+                    Atom.of("t-shirt"),
+                    Struct.of(
+                        "rgb",
+                        Integer.of(119),
+                        Integer.of(136),
+                        Integer.of(153)
+                    )
+                ).addLabel("summer").addLabel("spring")
+            ),
+            // dress (jeans , rgb (188 , 143 , 143) ) <@winter ,@fall , @summer , @spring >.
+            Fact.of(
+                Struct.of(
+                    "dress",
+                    Atom.of("jeans"),
+                    Struct.of(
+                        "rgb",
+                        Integer.of(255),
+                        Integer.of(220),
+                        Integer.of(210)
+                    )
+                ).addLabel("winter").addLabel("fall").addLabel("summer").addLabel("spring")
+            ),
         )
+
+        val solver = createSolver(theory)
 
         val solution = solver.solveOnce(goal)
         assertTrue(solution.isYes)
